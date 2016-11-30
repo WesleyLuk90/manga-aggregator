2094afa1b3e4ae5a649bf37158d62c29cf2c1da9
Wesley Luk <wesleyluk@wesleyluk.com>
2016-11-29 23:59:15 -0800

    Navigation formatting, switch to html5 mode

--- client/app.js
+++ client/app.js
@@ -3,11 +3,14 @@ import page from './page/page';
 import home from './home/home';
 
 require('angular-ui-router');
-
-angular.module('mangaApp', [
-    page,
-    home,
-    'ui.router',
-]);
+const app = angular
+    .module('mangaApp', [
+        page,
+        home,
+        'ui.router',
+    ]);
+app.config(($locationProvider) => {
+    $locationProvider.html5Mode(true);
+});
 
 angular.bootstrap(document.body, ['mangaApp'], { strictDi: true });
